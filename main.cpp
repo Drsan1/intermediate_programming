4c5a302433b9bb911a2b3bc190fd04234dffb043
Tomáš Kotulan <tkotulan10gmail.com>
2023-10-17 15:54:53 +0200

    lol

--- main.cpp
+++ main.cpp
@@ -16,12 +16,12 @@ Vector5 diff(Vector5 a, Vector5 b) {
     return {a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w, a.v - b.v};
 }
 
-Vector5 hamming(Vector5 a, Vector5 b) {
-    double x_sum = abs(a.x - b.x);
-    double y_sum = abs(a.y - b.y);
-    double z_sum = abs(a.z - b.z);
-    double w_sum = abs(a.w - b.w);
-    double v_sum = abs(a.v - b.v);
+int hamming(Vector5 a, Vector5 b) {
+    int x_sum = abs(a.x - b.x);
+    int y_sum = abs(a.y - b.y);
+    int z_sum = abs(a.z - b.z);
+    int w_sum = abs(a.w - b.w);
+    int v_sum = abs(a.v - b.v);
 
     return {x_sum + y_sum + z_sum + w_sum + v_sum};
 }
@@ -45,7 +45,7 @@ int main() {
     Vector5 vec2(3, 4, 5, 6, 1);
     Vector5 _sum = sum(vec1, vec2);
     Vector5 _diff = diff(vec1, vec2);
-    Vector5 _hamming = hamming(vec1, vec2);
+    int _hamming = hamming(vec1, vec2);
 
 
     std::cout << "vec1: (" << vec1.x << "," << vec1.y << "," << vec1.z << "," << vec1.w << "," << vec1.v << ")" << std::endl;
@@ -53,5 +53,5 @@ int main() {
     std::cout << std::endl;
     std::cout<<  "Sum: (" << _sum.x << "," << _sum.y << "," << _sum.z << "," << _sum.w << "," << _sum.v << ")" << std::endl;
     std::cout<<  "Diff: (" << _diff.x << "," << _diff.y << "," << _diff.z << "," << _diff.w << "," << _diff.v << ")" << std::endl;
-    std::cout << "Hamming: " << _hamming.x << std::endl;
+    std::cout << "Hamming: " << _hamming << std::endl;
 }
